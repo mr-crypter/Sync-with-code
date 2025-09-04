2a60d455e35957162b66d10d74b6b1b4bb6b8506
Om Saxena <38111516+mr-crypter@users.noreply.github.com>
2025-09-04 14:55:21 +0530

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, "public"))) // Serve static files
 const server = http.createServer(app)
 const io = new Server(server, {
 	cors: {
-		origin: "*",
+		origin: "https://sync-with-code.vercel.app",
 	},
 	maxHttpBufferSize: 1e8,
 	pingTimeout: 60000,
